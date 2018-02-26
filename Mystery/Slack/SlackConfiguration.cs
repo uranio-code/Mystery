114fe8e7a324bf59b21d9d81bdccfb5eaf6f62d7
uranio <carlo.capuano@hotmail.com>
2018-02-26 08:48:48 +0100

    small Slack integration

---
+++ Mystery/Slack/SlackConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mystery.Slack
+{
+    /// <summary>
+    /// uses Incoming WebHooks application of slack
+    /// </summary>
+    public class SlackConfiguration
+    {
+        public string Webhook_URL { get; set; }
+    }
+}
---
+++ Mystery/Slack/SlackMessager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Mystery.Configuration;
+using Mystery.Json;
+using Mystery.Register;
+using Mystery.Web;
+
+namespace Mystery.Slack
+{
+
+    public class SlackMessage {
+        public string text { get; set; }
+    }
+
+    [GlobalAvalibleObjectImplementation(singleton = true)]
+    public class SlackMessager
+    {
+        public bool SendMessage(string message) {
+            var cf = this.getGlobalObject<IConfigurationProvider>();
+            var conf = cf.getConfiguration<SlackConfiguration>();
+            if (string.IsNullOrWhiteSpace(conf.Webhook_URL))
+                return false;
+            var c = new WebClient();
+            var slack_message = new SlackMessage() { text = message };
+            var jc = this.getGlobalObject<MysteryJsonConverter>();
+            var json = jc.getJson(slack_message);
+            var result = c.UploadString(conf.Webhook_URL, json);
+            return true;
+        }
+    }
+}
