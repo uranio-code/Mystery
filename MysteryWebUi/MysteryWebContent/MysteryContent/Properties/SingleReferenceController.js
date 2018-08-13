87c3cf25a6297cf043c0b7a09b8e2fb940531e68
carlo capuano <carlo.capuano@iter.org>
2018-08-13 15:55:29 +0200

    quite weak attempt to make edit control only, they are all to be redone

--- MysteryWebUi/MysteryWebContent/MysteryContent/Properties/SingleReferenceController.js
+++ MysteryWebUi/MysteryWebContent/MysteryContent/Properties/SingleReferenceController.js
@@ -10,26 +10,58 @@ app.controller("SingleReferenceController", ['MysteryDownloader', '$scope', '$ui
     me.avalible = [];
     me.cp = null;
 
-    me.init = function (cp) {
-        me.cp = cp;
-        if (cp.data.can_edit) {
-            me.service_url =
-            'ContentEditService/SingleReferenceSuggestions/' +
-            cp.data.content.ContentType + '/' +
-            cp.data.content.tiny_guid + '/' +
-            cp.data.name;
+    me.getSuggestionsUlr = function () {
+        return 'ContentEditService/SingleReferenceSuggestions/' +
+            me.cp.data.content.ContentType + '/' +
+            me.cp.data.content.tiny_guid + '/' +
+            me.cp.data.name;
+    }
+
+    me.loadDataFromPropertyControl = function () {
+        if (me.cp.data.can_edit) {
+            me.service_url = me.getSuggestionsUlr();
+
             MysteryDownloader.get(me.service_url, function (data) {
                 me.avalible = data.output;
             });
         };
-        if (cp.data.content[cp.data.name] !==null)
-        {
-            me.selected_content = cp.data.content[cp.data.name];
+        if (me.cp.data.content[me.cp.data.name] !== null) {
+            me.selected_content = me.cp.data.content[me.cp.data.name];
             me.selected = false;
             me.original_selection = me.selected_content;
         }
+    }
+
+    //anysync loading
+    me.getAvalible = function (search_string) {
+        if (search_string && angular.isString(search_string)) {
+            return MysteryDownloader.getPromise(me.service_url + '?st=' + encodeURI(search_string), function (data) { return data.output; });
+        }
+        else {
+            return MysteryDownloader.getPromise(me.service_url, function (data) { return data.output; });
+        }
+    }
+
+    me.init = function (cp) {
+        me.cp = cp;
+        if (angular.isDefined(cp.data)) {
+            me.loadDataFromPropertyControl();
+        }
+        else {
+            //property controller have no data yet, in this cases the function
+            //initWithContent will be called when the content is loaded
+            //we add ourself to it
+            var current_initWithContent = me.cp.initWithContent;
+            me.cp.initWithContent = function (content, property_name) {
+                current_initWithContent(content, property_name);
+                me.loadDataFromPropertyControl();
+            }
+
+        }
+        
         
     };
+
 
     me.removeSelection = function () {
         me.selected_content = { guid: '', ReferenceText: '' }
@@ -44,7 +76,7 @@ app.controller("SingleReferenceController", ['MysteryDownloader', '$scope', '$ui
     }
 
     me.confirmSelection = function () {
-        if (me.selected_content == null) {
+        if (me.selected_content === null) {
             me.selected_content = me.original_selection;
             me.cp.current_value = { guid: me.original_selection.guid, ContentType: me.original_selection.ContentType };
         }
